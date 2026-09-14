8fb8f305301a43500dd7a4d88dfd33a4dcd0db0b
binghampamela2-star <binghampamela2@gmail.com>
2026-09-14 12:14:59 +0100

    my first commit

---
+++ script.js
@@ -0,0 +1,246 @@
+// =====================================
+// PUT YOUR API HERE
+// =====================================
+
+// Replace this with your API key
+const API_KEY = "d21f10991a75693df2e960814094b901";
+
+// Replace this with your API URL
+const API_URL = "https://api.themoviedb.org/3/search/movie?query=Batman&api_key=d21f10991a75693df2e960814094b901";
+
+
+const searchInput = document.getElementById("searchInput");
+
+const searchButton = document.getElementById("searchButton");
+
+const trailerButton = document.getElementById("trailerButton");
+
+const playButton = document.getElementById("playButton");
+
+const listButton = document.getElementById("listButton");
+
+const tabs = document.querySelectorAll(".tab");
+
+const tabContents = document.querySelectorAll(".tab-content");
+
+
+// SEARCH BUTTON
+
+searchButton.addEventListener("click", function () {
+
+    const movieName = searchInput.value;
+
+    if (movieName === "") {
+        alert("Please type a movie name");
+        return;
+    }
+
+    searchMovie(movieName);
+
+});
+
+
+
+// SEARCH WHEN ENTER IS PRESSED
+
+
+searchInput.addEventListener("keypress", function (event) {
+
+    if (event.key === "Enter") {
+
+        searchButton.click();
+
+    }
+
+});
+
+// SEARCH MOVIE USING API
+
+async function searchMovie(movieName) {
+
+    try {
+
+        /*
+        PUT YOUR API REQUEST HERE.
+
+        For example, if you are using TMDB:
+
+        const url =
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${movieName}`;
+
+        */
+
+        const url =
+            `${API_URL}?api_key=${API_KEY}&query=${movieName}`;
+
+
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+        console.log(data);
+
+        // You can use the information from your API here.
+
+        if (data.results && data.results.length > 0) {
+
+            const movie = data.results[0];
+
+            document.getElementById("movieTitle").textContent =
+                movie.title;
+
+            document.getElementById("movieRating").textContent =
+                movie.vote_average;
+
+            document.getElementById("movieYear").textContent =
+                movie.release_date;
+
+        } else {
+
+            alert("Movie not found");
+
+        }
+
+    } catch (error) {
+
+        console.log(error);
+
+        alert("Something went wrong with the API");
+
+    }
+
+}
+
+
+// WATCH TRAILER BUTTON
+
+trailerButton.addEventListener("click", function () {
+
+    window.open(
+        "https://www.youtube.com/results?search_query=Dune+Part+Two+trailer",
+        "_blank"
+    );
+
+});
+
+
+// PLAY BUTTON
+
+playButton.addEventListener("click", function () {
+
+    window.open(
+        "https://www.youtube.com/results?search_query=Dune+Part+Two+official+trailer",
+        "_blank"
+    );
+
+});
+
+// MY LIST BUTTON
+
+let addedToList = false;
+
+listButton.addEventListener("click", function () {
+
+    if (addedToList === false) {
+
+        listButton.textContent = "✓ Added to My List";
+
+        addedToList = true;
+
+    } else {
+
+        listButton.textContent = "＋ My List";
+
+        addedToList = false;
+
+    }
+
+});
+
+// TABS
+
+tabs.forEach(function (tab) {
+
+    tab.addEventListener("click", function () {
+
+        // Remove active from every button
+
+        tabs.forEach(function (button) {
+
+            button.classList.remove("active");
+
+        });
+
+
+        // Add active to clicked button
+
+        tab.classList.add("active");
+
+
+        // Hide every section
+
+        tabContents.forEach(function (content) {
+
+            content.classList.add("hidden");
+
+        });
+
+
+        // Get the section we want
+
+        const tabName = tab.getAttribute("data-tab");
+
+        const selectedContent =
+            document.getElementById(tabName);
+
+
+        // Show selected section
+
+        selectedContent.classList.remove("hidden");
+
+    });
+
+});
+
+
+
+// SIMILAR MOVIE CLICK
+
+const similarMovies =
+    document.querySelectorAll(".similar-movie");
+
+
+similarMovies.forEach(function (movie) {
+
+    movie.addEventListener("click", function () {
+
+        const movieTitle =
+            movie.querySelector("h3").textContent;
+
+        searchInput.value = movieTitle;
+
+        searchMovie(movieTitle);
+
+    });
+
+});
+
+
+
+// VIEW CAST
+
+document.getElementById("viewCast")
+    .addEventListener("click", function () {
+
+        document.querySelector('[data-tab="cast"]').click();
+
+    });
+
+// VIEW SIMILAR MOVIES
+
+document.getElementById("viewSimilar")
+    .addEventListener("click", function () {
+
+        document.querySelector('[data-tab="similar"]').click();
+
+    });
